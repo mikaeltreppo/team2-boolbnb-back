a7ecf862f215adf840b32ada77beae8054f73698
mikaeltreppo <mikaeltreppo@outlook.com>
2023-06-19 15:14:54 +0200

    validazione registrazione live

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,8 +32,11 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'lastname' => ['required', 'string','max:255'],
+            'phone' => ['required', 'string','max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            ''
         ]);
 
         $user = User::create([
