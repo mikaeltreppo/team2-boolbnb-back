4770a01ad023d4289aa8ef2b5a0f5d2013292977
Irene Brignoli <ire.brignoli@gmail.com>
2023-06-20 16:00:34 +0200

    inzizio show

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -1,8 +1,25 @@
 @extends('layouts.admin')
 @section('content')
     <div class="d-flex justify-content-center ">
-        <div class="card" style="width: 30rem;">
-            <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top" alt="...">
+        <div class="card">
+            <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top" alt="{{ $apartment->title }}">
+
+            <div class="card-body">
+                <h5 class="card-title fs-5 font-semibold">{{ $apartment->title }}</h5>
+                <span class="card-text"><i
+                    class="fa-solid fa-location-dot ms_light_gray_text me-2 mt-1"
+                  ></i>{{ $apartment->address }}</span>
+                
+                <div class="d-flex mt-2 justify-content-between">
+                    <div class="card-text">
+                        <span class="fw-bold ">{{ $apartment->price }}</span> €/notte
+                    </div>
+                    <div class="card-text">
+                        <span class="fw-bold ">{{ $apartment->size_m2 }}</span> m<sup>2</sup>
+                    </div>
+                </div>
+            </div>
+
             <div class="card-body">
                 <h5 class="card-title">{{ $apartment->title }}</h5>
                 <p class="card-text">{{ $apartment->description }}</p>
