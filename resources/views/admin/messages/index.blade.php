dc0c4599e66405570e0d68d012945676c810ef35
mattiamoneta <in.mattiamoneta@gmail.com>
2023-06-21 16:43:15 +0200

    Vista Messaggi OK

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -1,30 +1,43 @@
 @extends('layouts.admin')
 @section('content')
 
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Messaggio</th>
-        <th scope="col">Mittente</th>
-        <th scope="col">Per l'appartamento</th>
-        <th scope="col">Vedi</th>
+<h1 class="text-center">Messaggi</h1>
+<p class="text-center mb-5">Nuovi clienti ti stanno aspettando!</p>
 
-  
-      </tr>
-    </thead>
-    <tbody>
-        @foreach ($messages as $message)
-      <tr>
-        <th scope="row">{{$message->id}}</th>
-        <td>{{$message->message}}</td>
-        <td>{{$message->email}}</td>
-        <td>{{$message->apartment->name}}</td>
-        <td> <a href="{{route('admin.messages.show', ['message' => $message->id])}}"class="btn btn-primary">Vedi</a></td>
-   
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
+<div class="row">
+  <div class="col-10 mx-auto">
+    <div class="card card-tile flat-shadow">
+      <div class="card-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Messaggio</th>
+              <th scope="col">Mittente</th>
+              <th scope="col">Appartamento</th>
+              <th scope="col" class="text-center">Visualizza</th>
+        
+            </tr>
+          </thead>
+          <tbody>
+              @foreach ($messages as $message)
+            <tr>
+              <th scope="row">{{$message->id}}</th>
+              <td>{{$message->message}}</td>
+              <td>{{$message->email}}</td>
+              <td>{{$message->apartment->title}}</td>
+              <td class="text-center"> <a href="{{route('admin.messages.show', ['message' => $message->id])}}"class="btn btn-sm ms-btn-primary">
+                <i class="fa-solid fa-eye me-1"></i>
+                Vedi
+                </a>
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection
--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -1,19 +1,32 @@
 @extends('layouts.admin')
 @section('content')
-<div class="d-flex justify-content-center ">
-    <div class="card" style="width: 30rem;">
-        <img src="https://picsum.photos/400/250" class="card-img-top" alt="...">
-        <div class="card-body">
- 
-            <p class="card-text">{{$message->message}}</p>
-       
-            
-            <form method="POST" action="{{ route('admin.messages.destroy', ['message' => $message->id]) }}">
-                @method('DELETE')
-                @csrf
-                <button type="submit" class="btn btn-danger mt-2">Elimina Messaggio</button>
-            </form>
+
+<h1 class="text-center">Messaggi</h1>
+<p class="text-center mb-5">Nuovi clienti ti stanno aspettando!</p>
+
+<div class="row">
+    <div class="col-8 mx-auto">
+        <div class="card card-tile flat-shadow rounded-4">
+            <div class="card-header">
+                <div class="small fw-bold">Da: <span class="fw-lighter">{{$message->full_name}}</span></div>
+                <div class="small fw-bold">Appartamento: <span class="fw-lighter">{{$message->apartment->title}}</span></div>
+            </div>
+            <div class="card-body">
+                <div class="small fw-bold">Messaggio:</div>
+                <p class="card-text">{{$message->message}}</p>
+            </div>
+            <div class="card-footer">
+                <form method="POST" action="{{ route('admin.messages.destroy', ['message' => $message->id]) }}">
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit" class="btn btn-sm ms-btn-primary mt-2">
+                        <i class="fa-solid fa-envelope-circle-check me-1"></i>
+                        Segna come letto
+                    </button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
+
 @endsection
