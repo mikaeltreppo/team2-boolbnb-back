ececda713fb84bc91dd138a26ca1f07437a455fb
mattiamoneta <in.mattiamoneta@gmail.com>
2023-06-21 15:10:37 +0200

    Ok Sponsorships

--- resources/views/admin/sponsorships/index.blade.php
+++ resources/views/admin/sponsorships/index.blade.php
@@ -13,10 +13,13 @@
             @method('POST')
 
             <div class="row mt-5">
-                <div class="col-5 mx-auto text-center">
-                    <label for="apartmentSelect" class="mb-2 small">Scegli un appartamento</label>
-                    <select class="form-select form-select mb-3" aria-label=".form-select-lg example" id="apartmentSelect">
-                        <option selected>Open this select menu</option>
+                <div class="col-5 mx-auto text-center mb-4">
+                    <label for="apartmentSelect" class="mb-2 small">
+                        <span class="number-badge me-2">1</span>
+                        Scegli un <span class="fw-bold">appartamento</span>
+                    </label>
+                    <select class="form-select form-select-sm mb-3 mt-3" aria-label=".form-select-lg example" id="apartmentSelect">
+                        <option selected>-</option>
                         @foreach ($apartments as $apartment)         
                             <option value="{{$apartment->id}}" name="apartment_id">{{$apartment->title}}</option>
                         @endforeach
@@ -26,49 +29,49 @@
             </div>
 
             <div class="row py-3 justify-content-center">
+                
+                <span class="small text-center mb-3"><span class="number-badge me-2">2</span> Scegli un <span class="fw-bold">piano di sponsorizzazione</span></span>
+
                 @foreach ($sponsorships as $sponsorship)
                 <div class="col-3">
-                    <div class="card card-tile flat-shadow m-3 drop-shadow-sm">
-                        <div class="card-header py-4 ms-bg-dark text-light">
-                            {{-- <div class="rounded-icon mx-auto bg-primary my-3">
-                                <i class="fa-solid fa-eye"></i>
-                            </div> --}}
-                            <h6 class="text-center font-secondary text-light small text-uppercase fw-bold">{{ $sponsorship->name }}</h6>
+                    <label class="card-radio">
+                        <input type="radio" id="sponsorship_{{$sponsorship->id}}" name="sponsorship_id" value="{{$sponsorship->id}}" class="card-radio-element">
 
-                            <h1 class="fw-semibold text-center mb-0">
-                                {{ $sponsorship->price }}
-                            </h1>
-                            <h6 class="text-center font-secondary small text-uppercase fw-bold">{{ $sponsorship->duration }} h</h6>
-                        </div>
-                        <div class="card-body text-center">
-                            
-                           <ul class="list-unstyled">
-                                <li class="my-3">
-                                    Feature #1
-                                </li>
-                                <li class="my-3">
-                                    Feature #2
-                                </li>
-                                <li class="my-3">
-                                    Feature #3
-                                </li>
-                           </ul>
+                        <div class="card card-tile flat-shadow m-3 drop-shadow-sm card-radio-content">
+                            <div class="card-header py-4 bg-secondary text-light">
 
-                            {{-- <h5 class="card-title">{{ $sponsorship->name }}</h5>
-                            <h5 class="card-title">{{ $sponsorship->price }} €</h5>
-                            <h5 class="card-title">{{ $sponsorship->duration }} Ore</h5> --}}
-                            <input type="radio" id="sponsorship_{{$sponsorship->id}}" name="sponsorship_id" value="{{$sponsorship->id}}">
+                                <h6 class="text-center font-secondary text-light small text-uppercase fw-bold">{{ $sponsorship->name }}</h6>
+    
+                                <h1 class="fw-semibold text-center mb-0">
+                                    {{ $sponsorship->price }}
+                                </h1>
+                                <h6 class="text-center font-secondary small text-uppercase fw-bold">{{ $sponsorship->duration }} h</h6>
+                            </div>
+                            <div class="card-body text-center">
+                                
+                               <ul class="list-unstyled">
+                                    <li class="my-3">
+                                        Feature #1
+                                    </li>
+                                    <li class="my-3">
+                                        Feature #2
+                                    </li>
+                                    <li class="my-3">
+                                        Feature #3
+                                    </li>
+                               </ul>
+                               
+                            </div>
                         </div>
-                    </div>
+                    </label>
                 </div>
             @endforeach
             </div>
-              
         
               
              <div class="row">
                 <div class="col text-center my-5">
-                    <button type="submit" class="btn ms-btn ms-btn-primary">Sponsorizza</button>
+                    <button type="submit" class="btn ms-btn ms-btn-primary">Continua</button>
                 </div>
              </div>
         </form>
