5a10de2bde37ee0083f990e08a4532a430f7d343
mattiamoneta <in.mattiamoneta@gmail.com>
2023-06-21 11:36:58 +0200

    Error Pages

--- resources/views/errors/402.blade.php
+++ resources/views/errors/402.blade.php
@@ -1,1 +1,17 @@
-Errore 402
+@extends('layouts.errors')
+@section('content')
+<div class="container">
+       <div class="row vh-100 align-items-center">
+        <div class="col-5 mx-auto text-center">
+            <h3 class="fw-secondary fw-bold display-4 text-muted">:(</h3>
+            <h1 class="error-page-title mb-0">403</h1>
+            <h5 class="font-secondary mb-5">Payment Required</h5>
+            <a href="{{ route('admin.dashboard') }}" class="btn ms-btn ms-btn-outline-primary">
+                <i class="fa-solid fa-house me-3"></i>
+                Dashboard
+            </a>
+        </div>
+       </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/errors.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    {{-- Google Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;700&family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
+
+    {{-- Fontawesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Usando Vite -->
+    @vite(['resources/js/app.js'])
+</head>
+
+    <body>
+        
+        @yield('content')
+
+    </body>
+
+</html>
