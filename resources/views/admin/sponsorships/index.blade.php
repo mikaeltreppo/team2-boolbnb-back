6a4d0fba85dec6d770ee6b3ae7fa3605c1bd063b
mattiamoneta <in.mattiamoneta@gmail.com>
2023-06-21 10:07:08 +0200

    Changed Index

--- resources/views/admin/sponsorships/index.blade.php
+++ resources/views/admin/sponsorships/index.blade.php
@@ -1,13 +1,4 @@
 @extends('layouts.admin')
 @section('content')
-@foreach ($sponsorships as $sponsorhip)
-<div class="card m-3" style="width: 14rem;">
-   
-    <div class="card-body">
-        <h5 class="card-title">{{ $sponsorhip->name }}</h5>
-        <h5 class="card-title">{{ $sponsorhip->price }} €</h5>
-        <h5 class="card-title">{{ $sponsorhip->duration }} Ore</h5>
-    </div>
-</div>
-@endforeach
+
 @endsection
