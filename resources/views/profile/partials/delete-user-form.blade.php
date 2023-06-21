20d0df065603fd00e5f8af644b4f815ec6f9acb9
mattiamoneta <in.mattiamoneta@gmail.com>
2023-06-21 16:23:34 +0200

    Modifica interfaccia pagine modifica account

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -1,17 +1,18 @@
 <section class="space-y-6">
-    <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Delete Account') }}
+    <header  class="mb-4">
+        <h2 class="text-lg font-medium text-gray-900 font-secondary">
+            {{ __('Elimina Account') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            {{ __('Una volta cancellato l\'account, tutte le risorse e i dati saranno eliminati in modo permanente. Prima di eliminare l\'account, scaricare tutti i dati e le informazioni che si desidera conservare.') }}
         </p>
     </header>
 
     <!-- Modal trigger button -->
-    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-account">
-        {{__('Delete Account')}}
+    <button type="button" class="btn ms-btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#delete-account">
+        <i class="fa-solid fa-trash-can me-1"></i>
+        {{__('Elimina')}}
     </button>
 
     <!-- Modal Body -->
@@ -20,7 +21,7 @@
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="delete-account">Delete Account</h5>
+                    <h5 class="modal-title font-secondary" id="delete-account">Delete Account</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -52,6 +53,7 @@
 
 
                             <button type="submit" class="btn btn-danger">
+                                <i class="fa-solid fa-trash-can me-3"></i>
                                 {{ __('Delete Account') }}
                             </button>
                             <!--  -->
