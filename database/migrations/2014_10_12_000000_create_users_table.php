4e94069fc1792e8514ddcbfb084bee3032217859
nicolo fossati <nicofossa01@gmail.com>
2023-06-21 14:53:29 +0200

    profile_image nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -17,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('lastname');
-            $table->string('profile_image');
+            $table->string('profile_image')->nullable();
             $table->string('email')->unique();
             $table->string('phone', 15)->unique();
             $table->timestamp('email_verified_at')->nullable();
