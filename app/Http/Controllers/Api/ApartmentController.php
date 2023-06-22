5504dba38dc31ea6e3e8503f6fe1c618cc16b8dc
nicolo fossati <nicofossa01@gmail.com>
2023-06-22 14:43:54 +0200

    rotta search da finire

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -8,13 +8,28 @@ use Illuminate\Http\Request;
 
 class ApartmentController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
-        $apartments = Apartment::with(['facilities','sponsorships','user','messages'])->paginate(6);
-        
+        $apartments = Apartment::with(['facilities', 'sponsorships', 'user', 'messages'])->paginate(6);
+
         return response()->json([
-            'success'=> true,
-            'results'=> $apartments,
+            'success' => true,
+            'results' => $apartments,
+        ]);
+    }
+
+    public function search(Request $request, $search) //ricerca sul title dell'appartamento
+    {
+        // Esegui la logica di ricerca utilizzando il parametro $search
+        // Ad esempio, puoi utilizzare il parametro come parte di una query per filtrare i risultati del database
+
+        $apartments = Apartment::where('title', 'like', '%' . $search . '%')->paginate(6);
+
+        // Restituisci i risultati come risposta JSON
+        return response()->json([
+            'success' => true,
+            'results' => $apartments
         ]);
     }
 }
