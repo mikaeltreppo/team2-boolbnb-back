0c2ee3c7d85ec1d989322554a5f2aa8202411ac0
Irene Brignoli <ire.brignoli@gmail.com>
2023-06-28 16:45:21 +0200

    AAA

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -39,7 +39,7 @@ class MessageController extends Controller
         $newMessage->fill($data);
         $newMessage->save();
 
-        Mail::to('info@owner.it')->send(new Newcontact($newMessage));
+        ///Mail::to('info@owner.it')->send(new Newcontact($newMessage));
 
         return response()->json(
             [
