810aaee96e4e369406d13472970447f3aa67151b
Simone Carcagnì <simonecarcagni@libero.it>
2023-06-29 10:58:22 +0200

    Pagamento Sponsorship Funzionamente

--- app/Http/Controllers/Admin/SponsorshipController.php
+++ app/Http/Controllers/Admin/SponsorshipController.php
@@ -6,6 +6,7 @@ use App\Models\Apartment;
 use App\Models\Sponsorship;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Validator;
 use Braintree;
 
 
@@ -18,26 +19,15 @@ class SponsorshipController extends Controller
      */
     public function index()
     {
-        /* Variabili dell'account Braintree */
-        $gateway = new Braintree\Gateway([
-            'environment' => config('services.braintree.environment'),
-            'merchantId' => config('services.braintree.merchantId'),
-            'publicKey' => config('services.braintree.publicKey'),
-            'privateKey' => config('services.braintree.privateKey')
-        ]);
-
-        /* Genero un Token per autorizzare il pagamento */
-        $token = $gateway->ClientToken()->generate();
-
-
         $sponsorships = Sponsorship::all();
         $user = auth()->user();
         $apartments = $user->apartments;
 
-        return view('admin.sponsorships.index', compact('sponsorships', 'apartments','token','gateway')); //Passo sia il token che le variabili
+        return view('admin.sponsorships.index', compact('sponsorships', 'apartments')); //Passo sia il token che le variabili
     }
 
-    public function checkouts(Request $request){
+    public function checkouts(Request $request)
+    {
 
         $gateway = new Braintree\Gateway([
             'environment' => config('services.braintree.environment'),
@@ -45,7 +35,6 @@ class SponsorshipController extends Controller
             'publicKey' => config('services.braintree.publicKey'),
             'privateKey' => config('services.braintree.privateKey')
         ]);
-
 
         $amount = $request->amount;
         $nonce = $request->payment_method_nonce;
@@ -63,12 +52,10 @@ class SponsorshipController extends Controller
             $transaction = $result->transaction;
 
             return view('admin.sponsorships.checkouts', ['transaction' => $transaction->id]);
-           
-                 
         } else {  // Se il pagamento no va a buon fine
             $errorString = "";
 
-            foreach($result->errors->deepAll() as $error) {
+            foreach ($result->errors->deepAll() as $error) {
                 $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
             }
 
@@ -94,6 +81,26 @@ class SponsorshipController extends Controller
      */
     public function store(Request $request)
     {
+        $data = $request->all();
+
+        // Trovo l'id riferito all'appartamento selezionato e tutti i suoi dati
+        $apartment = Apartment::findOrFail($data['apartment_id']);
+
+        $validator = Validator::make($data, [
+            'sponsorship_id' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->with('status', 'Campo mancante')->withErrors($validator)->withInput();
+        }
+
+        $apartment->apartment_sponsorship()->attach([$data['sponsorship_id']]);
+
+        // Trovo l'id della promozione selezionata
+        $sponsorship = Sponsorship::findOrFail($data['sponsorship_id']);
+
+
+        return redirect()->route('admin.sponsorships.show', $sponsorship->id);
     }
 
     /**
@@ -104,8 +111,18 @@ class SponsorshipController extends Controller
      */
     public function show(Request $request, $sponsorship_id)
     {
-        
-     
+        /* Variabili dell'account Braintree */
+        $gateway = new Braintree\Gateway([
+            'environment' => config('services.braintree.environment'),
+            'merchantId' => config('services.braintree.merchantId'),
+            'publicKey' => config('services.braintree.publicKey'),
+            'privateKey' => config('services.braintree.privateKey')
+        ]);
+
+        /* Genero un Token per autorizzare il pagamento */
+        $token = $gateway->ClientToken()->generate();
+
+        return view('admin.sponsorships.show', compact('token', 'gateway'));
     }
 
     /**
@@ -130,7 +147,7 @@ class SponsorshipController extends Controller
     {
         //
     }
-    
+
 
     /**
      * Remove the specified resource from storage.
@@ -143,7 +160,7 @@ class SponsorshipController extends Controller
         //
     }
 
-    public function salvaRelazione(Request $request)
+    /*  public function salvaRelazione(Request $request)
     {
 
         $apartmentId = $request->input('apartment_id');
@@ -165,7 +182,7 @@ class SponsorshipController extends Controller
             'start_date' => $start_date,
             'expired_at' => $expired_at,
         ]);
-        */
+        
 
 
         return redirect()->back()->with([
@@ -175,5 +192,5 @@ class SponsorshipController extends Controller
             'start_date' => $start_date,
             'expired_date' => $expired_at
         ]);
-    }
+    }*/
 }
--- resources/views/admin/sponsorships/index.blade.php
+++ resources/views/admin/sponsorships/index.blade.php
@@ -1,144 +1,86 @@
 @extends('layouts.admin')
 @section('content')
+    <div class="row">
+        <div class="col-10 mx-auto">
+
+            <h1 class=" text-center mb-2">Sponsorizza</h1>
+            <p class="text-center">Dai una marcia in più ai tuoi annunci!</p>
+
+            <form method="POST" action="{{ route('admin.sponsorships.store') }}">
+                @csrf
+                @method('POST')
+
+                <div class="row mt-5">
+                    <div class="col-5 mx-auto text-center mb-4">
+                        <label for="apartmentSelect" class="mb-2 small">
+                            <span class="number-badge me-2">1</span>
+                            Scegli un <span class="fw-bold">appartamento</span>
+                        </label>
+
+                        <select class="form-select form-select-sm mb-3 mt-3" aria-label=".form-select-lg example"
+                            id="apartmentSelect" name="apartment_id">
+                            <option selected>-</option>
+                            @foreach ($apartments as $apartment)
+                                <option value="{{ $apartment->id }}"
+                                    {{ old('apartment_id') == $apartment->id ? 'selected' : '' }}>{{ $apartment->title }}
+                                </option>
+                            @endforeach
+                        </select>
 
 
-<div class="row">
-    <div class="col-10 mx-auto">
-
-        <h1 class=" text-center mb-2">Sponsorizza</h1>
-        <p class="text-center">Dai una marcia in più ai tuoi annunci!</p>
-        
-        <form method="POST" action="salva-relazione">
-            @csrf
-            @method('POST')
-
-            <div class="row mt-5">
-                <div class="col-5 mx-auto text-center mb-4">
-                    <label for="apartmentSelect" class="mb-2 small">
-                        <span class="number-badge me-2">1</span>
-                        Scegli un <span class="fw-bold">appartamento</span>
-                    </label>
-                    
-                    <select class="form-select form-select-sm mb-3 mt-3" aria-label=".form-select-lg example" id="apartmentSelect" name="apartment_id">
-                        <option selected>-</option>
-                        @foreach ($apartments as $apartment)    
-                            <option value="{{ $apartment->id }}" {{ old('apartment_id') == $apartment->id ? 'selected' : '' }}>{{ $apartment->title }}</option>
-                        @endforeach
-                    </select>
-                      
-
-                </div>
-            
-            </div>
-
-            <div class="row py-3 justify-content-center">
-                
-                <span class="small text-center mb-3"><span class="number-badge me-2">2</span> Scegli un <span class="fw-bold">piano di sponsorizzazione</span></span>
-
-                @foreach ($sponsorships as $sponsorship)
-                <div class="col-3">
-                    <label class="card-radio">
-                        <input type="radio" id="sponsorship_{{$sponsorship->id}}" name="sponsorship_id" value="{{$sponsorship->id}}" class="card-radio-element">
-
-                        <div class="card card-tile flat-shadow m-3 drop-shadow-sm card-radio-content">
-                            <div class="card-header py-4 bg-secondary text-light">
-
-                                <h6 class="text-center font-secondary text-light small text-uppercase fw-bold">{{ $sponsorship->name }}</h6>
-    
-                                <h1 class="fw-semibold text-center mb-0">
-                                    {{ $sponsorship->price }}
-                                </h1>
-                                <h6 class="text-center font-secondary small text-uppercase fw-bold">{{ $sponsorship->duration }} h</h6>
-                            </div>
-                            <div class="card-body text-center">
-                                
-                               <ul class="list-unstyled">
-                                    <li class="my-3">
-                                        Feature #1
-                                    </li>
-                                    <li class="my-3">
-                                        Feature #2
-                                    </li>
-                                    <li class="my-3">
-                                        Feature #3
-                                    </li>
-                               </ul>
-                               
-                            </div>
-                        </div>
-                    </label>
-                </div>
-            @endforeach
-            </div>
-        
-              
-             <div class="row">
-                <div class="col text-center my-5">
-                    <button type="submit" class="btn ms-btn ms-btn-primary">Continua</button>
-                </div>
-             </div>
-        </form>
-
-        {{-- Form di pagamento --}}
-        <form method="post" id="payment-form" action="{{route('admin.sponsorships.checkouts')}}">
-            @csrf
-            <section>
-                {{-- Importo --}}
-                <label for="amount">
-                    <span class="input-label">Amount</span>
-                    <div class="input-wrapper amount-wrapper">
-                        <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
                     </div>
-                </label>
 
-                {{-- Render dell'interfaccia di pagamento Drop-In --}}
-                <div class="bt-drop-in-wrapper">
-                    <div id="bt-dropin"></div>
                 </div>
-            </section>
 
-            <input id="nonce" name="payment_method_nonce" type="hidden" />
-            <button class="button" type="submit"><span>Test Transaction</span></button>
-        </form>
-        {{-- Fine form di pagamento --}}
+                <div class="row py-3 justify-content-center">
+
+                    <span class="small text-center mb-3"><span class="number-badge me-2">2</span> Scegli un <span
+                            class="fw-bold">piano di sponsorizzazione</span></span>
+
+                    @foreach ($sponsorships as $sponsorship)
+                        <div class="col-3">
+                            <label class="card-radio">
+                                <input type="radio" id="sponsorship_{{ $sponsorship->id }}" name="sponsorship_id"
+                                    value="{{ $sponsorship->id }}" class="card-radio-element">
+
+                                <div class="card card-tile flat-shadow m-3 drop-shadow-sm card-radio-content">
+                                    <div class="card-header py-4 bg-secondary text-light">
+
+                                        <h6 class="text-center font-secondary text-light small text-uppercase fw-bold">
+                                            {{ $sponsorship->name }}</h6>
+
+                                        <h1 class="fw-semibold text-center mb-0">
+                                            {{ $sponsorship->price }}
+                                        </h1>
+                                        <h6 class="text-center font-secondary small text-uppercase fw-bold">
+                                            {{ $sponsorship->duration }} h</h6>
+                                    </div>
+                                    <div class="card-body text-center">
+
+                                        <ul class="list-unstyled">
+                                            <li class="my-3">
+                                                Feature #1
+                                            </li>
+                                            <li class="my-3">
+                                                Feature #2
+                                            </li>
+                                            <li class="my-3">
+                                                Feature #3
+                                            </li>
+                                        </ul>
+
+                                    </div>
+                                </div>
+                            </label>
+                        </div>
+                    @endforeach
+                </div>
 
 
-
-
-    </div>
-</div>
-
-<script src="https://js.braintreegateway.com/web/dropin/1.38.1/js/dropin.min.js"></script>
-<script>
-    var form = document.querySelector('#payment-form');
-    var client_token = "{{$token}}";
-
-    braintree.dropin.create({
-      authorization: client_token,
-      selector: '#bt-dropin',
-      paypal: {
-        flow: 'vault'
-      }
-    }, function (createErr, instance) {
-      if (createErr) {
-        console.log('Create Error', createErr);
-        return;
-      }
-      form.addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        instance.requestPaymentMethod(function (err, payload) {
-          if (err) {
-            console.log('Request Payment Method Error', err);
-            return;
-          }
-
-          // Add the nonce to the form and submit
-          document.querySelector('#nonce').value = payload.nonce;
-          form.submit();
-        });
-      });
-    });
-</script>
-    
-@endsection
+                <div class="row">
+                    <div class="col text-center my-5">
+                        <button type="submit" class="btn ms-btn ms-btn-primary">Continua</button>
+                    </div>
+                </div>
+            </form>
+        @endsection
