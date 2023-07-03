d3e5de008169cf37a4b964411d3bba255bce96db
mikaeltreppo <mikaeltreppo@outlook.com>
2023-07-03 11:34:21 +0200

    Input sistemata

--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -81,12 +81,12 @@
                                 {{ $message }}
                             </div>
                         @enderror
-                        <input type="hidden" id="longitude" name="longitude" value="">
-                        <input type="hidden" id="latitude" name="latitude" value="">
+                        <input type="hidden" id="longitude" name="longitude" value="{{ old('apartment', $apartment->longitude) }}">
+                        <input type="hidden" id="latitude" name="latitude" value="{{ old('apartment', $apartment->latitude) }}">
 
-                        <input type="hidden" id="address" name="address" value="">
-                        <input type="hidden" id="city" name="city" value="">
-                        <input type="hidden" id="country" name="country" value="">
+                        <input type="hidden" id="address" name="address" value="{{ old('apartment', $apartment->address) }}">
+                        <input type="hidden" id="city" name="city" value="{{ old('apartment', $apartment->city) }}">
+                        <input type="hidden" id="country" name="country" value="{{ old('apartment', $apartment->country) }}">
                     </div>
                    
                     <div class="p-3 col-12">
